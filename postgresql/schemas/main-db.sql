6e14359c075a5854257c015913690f11c29a0c07
Alexander <48819332+reosfire@users.noreply.github.com>
2025-06-05 00:58:24 +0300

    add decrease_user_balancer function

--- postgresql/schemas/main-db.sql
+++ postgresql/schemas/main-db.sql
@@ -30,3 +30,27 @@ CREATE TABLE projects_kv_storage (
     PRIMARY KEY (project_id, key),
     FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE
 );
+
+CREATE OR REPLACE FUNCTION decrease_user_balance(
+    p_user_id INT,
+    p_amount NUMERIC(30, 10)
+) RETURNS BOOLEAN AS $$
+DECLARE
+    v_sufficient_funds BOOLEAN;
+BEGIN
+    SELECT money_balance >= p_amount INTO v_sufficient_funds
+    FROM users
+    WHERE id = p_user_id
+    FOR UPDATE;
+    
+    IF NOT FOUND OR NOT v_sufficient_funds THEN
+        RETURN FALSE;
+    END IF;
+    
+    UPDATE users
+    SET money_balance = money_balance - p_amount
+    WHERE id = p_user_id;
+    
+    RETURN TRUE;
+END;
+$$ LANGUAGE plpgsql;
